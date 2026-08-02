cae5edf750f767218290f3d8e943b0622540f844
mayankpathar <mayankpathar49@gmail.com>
2026-08-02 17:57:45 +0530

    feat: reorganize imports and update component structure in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Header from './Header'
-
-import Footer from './Footer'
 import Herosection from './Herosection'
+import Latestmovies from './Latestmovies'
+import Latestseries from './Latestseries'
+import Latestanime from './Latestanime'
+import Specialmovie from './Specialmovie'
+import Pricingsection from './Pricingsection'
+import Footer from './Footer'
 
 function App() {
   return(<>
  <Header/>
  <Herosection/>
+ <Latestmovies/>
+ <Latestseries/>
+ <Latestanime/>
+ <Specialmovie/>
+ <Pricingsection/>
  <Footer/>
 
   </>);
