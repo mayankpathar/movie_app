6855f8505a3d316bd561ae785cc186d423d13b26
mayankpathar <mayankpathar49@gmail.com>
2026-08-02 17:18:14 +0530

    Add initial HTML structure and assets for Flix website, including hero section, movie listings, and pricing plans

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,66 +1,90 @@
 import React from 'react'
 
 function Footer() {
-  return (
+	return (
+		<footer class="section">
+			<div class="container">
+				<div class="row">
+					<div class="col-4 col-md-6 col-sm-12">
+						<div class="content">
+							<a href="#" class="logo">
+								<i class='bx bx-movie-play bx-tada main-color'></i>Fl<span class="main-color">i</span>x
+							</a>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut veniam ex quos hic id nobis beatae earum sapiente! Quod ipsa exercitationem officiis non error illum minima iusto et. Dolores, quibusdam?
+							</p>
+							<div class="social-list">
+								<a href="#" class="social-item">
+									<i class="bx bxl-facebook"></i>
+								</a>
+								<a href="#" class="social-item">
+									<i class="bx bxl-twitter"></i>
+								</a>
+								<a href="#" class="social-item">
+									<i class="bx bxl-instagram"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-8 col-md-6 col-sm-12">
+						<div class="row">
+							<div class="col-3 col-md-6 col-sm-6">
+								<div class="content">
+									<p><b>Flix</b></p>
+									<ul class="footer-menu">
+										<li><a href="#">About us</a></li>
+										<li><a href="#">My profile</a></li>
+										<li><a href="#">Pricing plans</a></li>
+										<li><a href="#">Contacts</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-3 col-md-6 col-sm-6">
+								<div class="content">
+									<p><b>Browse</b></p>
+									<ul class="footer-menu">
+										<li><a href="#">About us</a></li>
+										<li><a href="#">My profile</a></li>
+										<li><a href="#">Pricing plans</a></li>
+										<li><a href="#">Contacts</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-3 col-md-6 col-sm-6">
+								<div class="content">
+									<p><b>Help</b></p>
+									<ul class="footer-menu">
+										<li><a href="#">About us</a></li>
+										<li><a href="#">My profile</a></li>
+										<li><a href="#">Pricing plans</a></li>
+										<li><a href="#">Contacts</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-3 col-md-6 col-sm-6">
+								<div class="content">
+									<p><b>Download app</b></p>
+									<ul class="footer-menu">
+										<li>
+											<a href="#">
+												<img src="./images/google-play.png" alt="" />
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<img src="./images/app-store.png" alt="" />
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
 
-      <footer class="ht-footer">
-	<div class="container">
-		<div class="flex-parent-ft">
-			<div class="flex-child-ft item1">
-				 <a href="index-2.html"><img class="logo" src="images/logo1.png" alt="" /></a>
-				 <p>5th Avenue st, manhattan<br/>
-				New York, NY 10001</p>
-				<p>Call us: <a href="#">(+01) 202 342 6789</a></p>
-			</div>
-			<div class="flex-child-ft item2">
-				<h4>Resources</h4>
-				<ul>
-					<li><a href="#">About</a></li> 
-					<li><a href="#">Blockbuster</a></li>
-					<li><a href="#">Contact Us</a></li>
-					<li><a href="#">Forums</a></li>
-					<li><a href="#">Blog</a></li>
-					<li><a href="#">Help Center</a></li>
-				</ul>
-			</div>
-			<div class="flex-child-ft item3">
-				<h4>Legal</h4>
-				<ul>
-					<li><a href="#">Terms of Use</a></li> 
-					<li><a href="#">Privacy Policy</a></li>	
-					<li><a href="#">Security</a></li>
-				</ul>
-			</div>
-			<div class="flex-child-ft item4">
-				<h4>Account</h4>
-				<ul>
-					<li><a href="#">My Account</a></li> 
-					<li><a href="#">Watchlist</a></li>	
-					<li><a href="#">Collections</a></li>
-					<li><a href="#">User Guide</a></li>
-				</ul>
-			</div>
-			<div class="flex-child-ft item5">
-				<h4>Newsletter</h4>
-				<p>Subscribe to our newsletter system now <br/> to get latest news from us.</p>
-				<form action="#">
-					<input type="text" placeholder="Enter your email..."/>
-				</form>
-				<a href="#" class="btn">Subscribe now <i class="ion-ios-arrow-forward"></i></a>
-			</div>
-		</div>
-	</div>
-	<div class="ft-copyright">
-		<div class="ft-left">
-			<p><a target="_blank" href="https://www.templateshub.net">Templates Hub</a></p>
-		</div>
-		<div class="backtotop">
-			<p><a href="#" id="back-to-top">Back to top  <i class="ion-ios-arrow-thin-up"></i></a></p>
-		</div>
-	</div>
-</footer>
-
-  )
+	)
 }
 
 export default Footer
